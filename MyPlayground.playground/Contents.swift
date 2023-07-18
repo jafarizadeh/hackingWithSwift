108a7e36505f67710538206f4d14f78b9b5ff78b
Mehdi <mehdi0341@gmail.com>
2023-07-18 13:04:22 +0200

    ...

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,74 +1,13 @@
 import UIKit
 
-//class Dog {
-//    var name: String
-//    var breed: String
-//
-//    init (name: String, breed: String)
-//    {
-//        self.name = name
-//        self.breed = breed
-//    }
-//}
-//
-//
-//class Poodle: Dog {
-//    init (name: String){
-//        super.init (name: name, breed: "Poodle")
-//    }
-//}
-
-//final class Dog {
-//    func makeNoise(){
-//        print ("Woof!")
-//    }
-//}
-//
-//class Poodle: Dog {
-//    override func makeNoise(){
-//        print("Yip!")
-//    }
-//}
-//
-//let poppy = Poodle()
-//poppy.makeNoise()
-
-//struct Singer{
-//    var name = "Taylor"
-//}
-//
-//var singer = Singer ()
-//print(singer.name)
-//
-//var singer2 = singer
-//singer2.name = "Justin"
-//print(singer.name)
-
-//class Person{
-//    var name = "John"
-//
-//    init(){
-//        print("\(name) is alive!")
-//    }
-//
-//    func printGreeting(){
-//        print("Hello, I'm \(name)")
-//    }
-//
-//    deinit {
-//        print ("\(name) is no more!")
-//    }
-//}
-//
-//for _ in 1...3 {
-//    let person = Person()
-//    person.printGreeting()
-//}
-
-class Singer{
-    let name = "Taylor"
+protocol Identifiable {
+    var id : String { get set }
 }
 
-let taylor = Singer()
-taylor.name = "Ed"
-print(taylor.name)
+struct user: Identifiable {
+    var id : String
+}
+
+func displayID (thing: Identifiable){
+    print("My ID is \(thing.id)")
+}
